55a1709a96446b2e30ef8fb0b54da28d525fa6c7
Kevin <kevinhan710187@gmail.com>
2021-01-13 11:10:36 -0800

    reverse code Single number added name changed

--- src/java_practice.java
+++ src/java_practice.java
@@ -1,27 +1,29 @@
 public class java_practice {
-    public static void main(char[] s) {
+    public static void main(String args[]) {
+        char[] s = {'H','e','l','l','o'};
         int left = 0, right = s.length-1;
         while(left < right){
             char tmp = s[left];
             s[left++] = s[right];
             s[right--] = tmp ;
         }
+        System.out.println(s);
     }
 
 
-    public int singleNumber(int[] nums) {
-        List<Integer> my_list = new ArrayList<>();
-
-        for(int i : nums){
-            if(!my_list.contains(i)){
-                my_list.add(i);
-            }else{
-                my_list.remove(new Integer(i));
-            }
-        }
-
-        return my_list.get(0);
-    }
+//    public int singleNumber(int[] nums) {
+//        List<Integer> my_list = new ArrayList<>();
+//
+//        for(int i : nums){
+//            if(!my_list.contains(i)){
+//                my_list.add(i);
+//            }else{
+//                my_list.remove(new Integer(i));
+//            }
+//        }
+//
+//        return my_list.get(0);
+//    }
 
 }
 
